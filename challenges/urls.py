5ed9642053a4165da08dbb3056d4358be22ab2cd
Arthur Raulino Kretzer <arthur.raulino.kretzer@gmail.com>
2022-11-17 20:27:41 -0300

    Dynamic redirect path

--- challenges/urls.py
+++ challenges/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     # this configures a place holder named month to dynamically set the route. We specify the type for the input. In that way we can set different routes.
     path("<int:month>", views.monthly_challenge_by_number),
-    path("<str:month>", views.monthly_challenge),
+    path("<str:month>", views.monthly_challenge, name="month-challenge"),
 ]
